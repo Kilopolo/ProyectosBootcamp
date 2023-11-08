073802f04534e717912a86998fdafa0fd7213d2e
DavidSantosM <114166018+DavidSantosM@users.noreply.github.com>
2023-11-08 17:47:09 +0100

    Commit inicial de AppCenter

--- AppCentral/App.js
+++ AppCentral/App.js
@@ -1,20 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Text, View, Slider, Button } from 'react-native';
 
-export default function App() {
+const CreditSimulator = () => {
+  const [montoTotal, setMontoTotal] = useState(5000);
+  const [plazo, setPlazo] = useState(3);
+
+  const interes = 0.9798; // 97,98%
+
+  const handleMontoChange = value => {
+    setMontoTotal(value);
+  };
+
+  const handlePlazoChange = value => {
+    setPlazo(value);
+  };
+
+  const calculateCuota = () => {
+    const interesAmount = montoTotal * interes;
+    const totalAmount = montoTotal + interesAmount;
+    const cuota = totalAmount / plazo;
+    return cuota;
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text>Simula tu crédito</Text>
+
+      <View>
+        <Text>Monto Total: {montoTotal}</Text>
+        <Slider
+          minimumValue={5000}
+          maximumValue={50000}
+          step={100}
+          value={montoTotal}
+          onValueChange={handleMontoChange}
+        />
+      </View>
+
+      <View>
+        <Text>Plazo: {plazo}</Text>
+        <Slider
+          minimumValue={3}
+          maximumValue={24}
+          step={1}
+          value={plazo}
+          onValueChange={handlePlazoChange}
+        />
+      </View>
+
+      <Text>Resultado:</Text>
+      <Text>TOTAL A DEVOLVER EN CUOTAS: {montoTotal + montoTotal * interes}</Text>
+      <Text>CUOTA FIJA EN {plazo} PAGOS: {calculateCuota()}</Text>
+
+      <Button title="Obtener Crédito" onPress={() => console.log("Obtener crédito")} />
+      <Button title="Ver detalle de cuotas" onPress={() => console.log("Ver detalle de cuotas")} />
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default CreditSimulator;
