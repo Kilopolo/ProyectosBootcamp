cfd02d1e5bc2c912ae8e683bce639de9a78dd610
DavidSantosM <114166018+DavidSantosM@users.noreply.github.com>
2023-11-09 11:47:55 +0100

    subidita de menuScreen

--- AppCentral/modules/MenuScreen.js
+++ AppCentral/modules/MenuScreen.js
@@ -6,15 +6,18 @@ import CalculatorScreen from "./CalculatorScreen";
 import GameScreen from "./GameScreen";
 import CreditAppScreen from "./CreditAppScreen";
 import IMCPatri from "./IMCPatri";
+import IMCDavid from "./AppCalculadoraDavid/IMCDavid";
 
 const Stack = createNativeStackNavigator();
 {/* <Stack.Navigator initialRouteName="Menu"> */}
 {/* <Stack.Screen name="Menu" component={MenuScreen} /> */}
+
 const MenuScreen = () => {
   return (
     <Stack.Navigator  initialRouteName="CreditApp">
       <Stack.Screen name="Calculator" component={CalculatorScreen} />
       <Stack.Screen name="IMCPatri" component={IMCPatri} />
+      <Stack.Screen name="IMCDavid" component={IMCDavid} />
       <Stack.Screen name="Game" component={GameScreen} />
       <Stack.Screen name="CreditApp" component={CreditAppScreen} />
     </Stack.Navigator>
