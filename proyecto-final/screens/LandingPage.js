90d63acd547d158e701a5af4296a2f9f1db2a543
Kilopolo <34222746+Kilopolo@users.noreply.github.com>
2023-11-13 14:01:40 +0100

    Update LandingPage.js

--- proyecto-final/screens/LandingPage.js
+++ proyecto-final/screens/LandingPage.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { signInWithEmailAndPassword, getAuth } from 'firebase/auth';
 
 const LandingPage = ({ userName }) => {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>¡Bienvenido,  {userName}!</Text>
+      <Text style={styles.title}>¡Bienvenido,  {getAuth().currentUser.email}!</Text>
       <Text style={styles.message}>Estás en la pantalla siguiente.</Text>
     </View>
   );
