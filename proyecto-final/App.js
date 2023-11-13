e2c9d90274b6d6e751af3ef8b12a1745684618d8
Kilopolo <34222746+Kilopolo@users.noreply.github.com>
2023-11-13 12:41:03 +0100

    .

--- proyecto-final/App.js
+++ proyecto-final/App.js
@@ -2,7 +2,7 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-
+import { firestore, collection, getDocs } from "@firebase/firestore";
 //locales
 import MenuScreen from "./screens/MenuScreen";
 import CreateUserScreen from "./screens/CreateUserScreen";
--- proyecto-final/screens/MenuScreen.js
+++ proyecto-final/screens/MenuScreen.js
@@ -1,57 +1,85 @@
-import React,{useState} from "react";
-import { ScrollView, View, Text, Button , TouchableWithoutFeedback,Animated} from "react-native";
+import React, { useState } from "react";
+import {
+  ScrollView,
+  View,
+  Text,
+  Button,
+  TouchableWithoutFeedback,
+  Animated,
+} from "react-native";
 import styles from "./StyleMenuScreen";
- 
+import SocialLogins from "./ReactFirebaseSocialLogins";
+import { firestore, collection, getDocs } from "@firebase/firestore";
+
 const MenuScreen = ({ navigation }) => {
- 
   const [scaleValue] = useState(new Animated.Value(1));
- 
+
+  
+
   const buttonScale = () => {
     Animated.sequence([
       Animated.timing(scaleValue, {
         toValue: 0.8,
         duration: 100,
-        useNativeDriver: true
+        useNativeDriver: true,
       }),
       Animated.timing(scaleValue, {
         toValue: 1,
         duration: 100,
-        useNativeDriver: true
+        useNativeDriver: true,
       }),
     ]).start();
   };
- 
+
   return (
-    <ScrollView >
+    <ScrollView>
       <View style={styles.buttonContainer}>
-        <TouchableWithoutFeedback onPress={() => { buttonScale(); navigation.navigate("CreateUserScreen") }}>
-          <Animated.View style={[styles.button, { transform: [{ scale: scaleValue }] }]}>
+        <TouchableWithoutFeedback
+          onPress={() => {
+            buttonScale();
+            navigation.navigate("CreateUserScreen");
+          }}
+        >
+          <Animated.View
+            style={[styles.button, { transform: [{ scale: scaleValue }] }]}
+          >
             <Text style={styles.buttonText}>Crear Usuario</Text>
           </Animated.View>
         </TouchableWithoutFeedback>
       </View>
       <View style={styles.buttonContainer}>
-        <TouchableWithoutFeedback onPress={() => { buttonScale(); navigation.navigate("UsersList") }}>
-          <Animated.View style={[styles.button, { transform: [{ scale: scaleValue }] }]}>
+        <TouchableWithoutFeedback
+          onPress={() => {
+            buttonScale();
+            navigation.navigate("UsersList");
+          }}
+        >
+          <Animated.View
+            style={[styles.button, { transform: [{ scale: scaleValue }] }]}
+          >
             <Text style={styles.buttonText}>Lista de Usuarios</Text>
           </Animated.View>
         </TouchableWithoutFeedback>
       </View>
       <View style={styles.buttonContainer}>
-        <TouchableWithoutFeedback onPress={() => { buttonScale(); navigation.navigate("FetchMovies") }}>
-          <Animated.View style={[styles.button, { transform: [{ scale: scaleValue }] }]}>
+        <TouchableWithoutFeedback
+          onPress={() => {
+            buttonScale();
+            navigation.navigate("FetchMovies");
+          }}
+        >
+          <Animated.View
+            style={[styles.button, { transform: [{ scale: scaleValue }] }]}
+          >
             <Text style={styles.buttonText}>FetchMovies Example</Text>
           </Animated.View>
         </TouchableWithoutFeedback>
       </View>
 
+  {/*           <SocialLogins fireRef={firestore} /> */}
+
     </ScrollView>
   );
 };
 
-
-
-
-
- 
 export default MenuScreen;
