730d1e837f940a31ae45aa1667396ef4956528d4
DavidSantosM <114166018+DavidSantosM@users.noreply.github.com>
2023-11-14 13:31:19 +0100

    Modificaciones del splash

--- proyecto-final/screens/SplashScreen.js
+++ proyecto-final/screens/SplashScreen.js
@@ -1,50 +1,62 @@
 import { useEffect, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { ImageBackground, StyleSheet, Text, View } from 'react-native';
 
 export default function SplashScreen({ navigation }) {
-  const [counter, setCounter] = useState(7);
+  const [counter, setCounter] = useState(5);
 
   useEffect(() => {
     const interval = setInterval(() => {
       setCounter(prevCounter => {
         if (prevCounter === 1) {
           clearInterval(interval);
-          navigation.navigate('Menu'); // Redirige a la pantalla Menu
+          navigation.navigate('Menu'); 
         }
         return prevCounter - 1;
       });
     }, 1000);
 
-    return () => clearInterval(interval); // Limpia el intervalo al desmontar el componente
+    return () => clearInterval(interval); 
   }, [navigation]);
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Bienvenido</Text>
-      <Text style={styles.paragraph}>
-        En {counter} segundos accederás a tu portal político favorito
-      </Text>
-    </View>
+    <ImageBackground source={require('../assets/splash_image.png')} style={styles.background}>
+      <View style={styles.container}>
+        <View style={styles.topContainer}>
+          <Text style={styles.title}>Bienvenido</Text>
+        </View>
+        <Text style={styles.counter}>
+          En {counter} segundos accederás a tu portal político favorito
+        </Text>
+      </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+    alignItems: "center"
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
+    alignItems: 'center'
+  },
+  topContainer: {
+    flex: 1,
+    justifyContent: 'flex-end', 
     alignItems: 'center',
-    backgroundColor: '#FFC300',
-    padding: 8,
+    marginBottom: 40 
   },
   title: {
-    margin: 24,
     fontSize: 38,
     fontWeight: 'bold',
     textAlign: 'center',
-    color: '#FFF',
+    color: '#6A5ACD',
   },
-  paragraph: {
+  counter: {
     fontSize: 18,
     textAlign: 'center',
     color: '#FFF',
