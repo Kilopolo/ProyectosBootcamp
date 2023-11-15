df1a7506de3967ae28ad317428e0ef66a88cf127
DavidSantosM <114166018+DavidSantosM@users.noreply.github.com>
2023-11-15 11:45:49 +0100

    cambios

--- proyecto-final/screens/MenuScreen.js
+++ proyecto-final/screens/MenuScreen.js
@@ -10,18 +10,24 @@ import {
 import stylesMenu from "./StyleMenuScreen";
 import { getAuth } from "firebase/auth";
 import PartidosLists from "./PartidosList";
+import { collection, getDocs } from "@firebase/firestore";
+import { firestore } from "../database/firebase";
 
 const MenuScreen = ({ navigation }) => {
   const [scaleValue] = useState(new Animated.Value(1));
   const [authenticated, setAuhtenticated] = useState(false);
+  const [listaPartidos, setListaPartidos] = useState([]);
+  const [isLoading, setLoading] = useState(true);
 
   useEffect(() => {
+    fetchPartido();
     const user = getAuth().currentUser;
     if (user === null) {
       setAuhtenticated(false);
     } else {
       setAuhtenticated(true);
     }
+    
   }, []);
 
   const buttonScale = () => {
@@ -39,16 +45,38 @@ const MenuScreen = ({ navigation }) => {
     ]).start();
   };
 
+  const fetchPartido = async () => {
+    try {
+      const querySnapshot = await getDocs(collection(firestore, "parties"));
+      const partyData = [];
+
+      querySnapshot.forEach((doc) => {
+        partyData.push({ id: doc.id, ...doc.data() });
+      });
+
+      await setListaPartidos(partyData);
+     
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <ImageBackground
       source={require("../assets/BKG.png")} // Reemplaza 'tu_imagen_de_fondo.jpg' con la ruta de tu imagen
       style={stylesMenu.allMenuContainer}
     >
       <View style={stylesMenu.allMenuContainer}>
-
         <View style={stylesMenu.someContainer}>
           <ScrollView>
-            <PartidosLists />
+            {isLoading ? (
+              <Text>Loading...</Text>
+            ) : (
+              <PartidosLists listaPartidos={{ listaPartidos }} />
+            )}
+
             {/* <View style={styles.buttonContainer}>
               <TouchableWithoutFeedback
                 onPress={() => {
---
+++ proyecto-final/screens/StylePartido.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+const stylesParty = StyleSheet.create({
+
+container:{
+  flex: 1,
+  alignItems: "center",
+  justifyContent: "center",
+  backgroundColor: "white",
+  borderWidth: 1,
+  marginBottom:15,
+  borderColor:'grey'
+}
+});
+
+export default stylesParty;
